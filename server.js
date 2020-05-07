e68e4fec7d0ae04e893423078f422b9521dd0021
Jordan Devaney <jordandevaney@Jordans-MacBook-Air.local>
2020-05-07 11:17:27 -0400

    require cors

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const shortid = require('shortid');
+const cors = require('cors');
 
 const server = express();
 
 server.use(express.json());
+
+server.use(cors());
+
 let users = [];
 
 server.get('/', (req, res) => {
